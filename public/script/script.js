335fa50ff6da58dc88f3e9640fae3b54a3dcbce4
Anna Gouvêa <anna.gouvea@kumulus.com.br>
2020-10-27 14:39:11 -0300

    format code

--- public/script/script.js
+++ public/script/script.js
@@ -1,15 +1,13 @@
-function enterPesquisa(e){
+function enterPesquisa(e) {
+  if (e.keyCode == 13) {
+    var pesquisa = document.getElementById("pesquisa-header");
 
-    if (e.keyCode == 13) {
-        
-        var pesquisa = document.getElementById("pesquisa-header");
-
-        if(pesquisa.value == ""){
-            document.getElementById("result-pesquisa").innerHTML = "Pesquisa Vazia";
-        }
-        else {
-            document.getElementById("result-pesquisa").innerHTML = "Você pesquisou por: " + pesquisa.value;
-        }        
-        return false;
+    if (pesquisa.value == "") {
+      document.getElementById("result-pesquisa").innerHTML = "Pesquisa Vazia";
+    } else {
+      document.getElementById("result-pesquisa").innerHTML =
+        "Você pesquisou por: " + pesquisa.value;
     }
+    return false;
+  }
 }
