5e64f8c22aeb0dfd52f187a119f09b0ada618a8b
annajuliapg <annajuliap.g@hotmail.com>
2020-11-18 08:23:59 -0300

    front oficial

---
+++ front-end/assets/script/script.js
@@ -0,0 +1,13 @@
+function enterPesquisa(e) {
+  if (e.keyCode == 13) {
+    var pesquisa = document.getElementById("pesquisa-header");
+
+    if (pesquisa.value == "") {
+      document.getElementById("result-pesquisa").innerHTML = "Pesquisa Vazia";
+    } else {
+      document.getElementById("result-pesquisa").innerHTML =
+        "Você pesquisou por: " + pesquisa.value;
+    }
+    return false;
+  }
+}
