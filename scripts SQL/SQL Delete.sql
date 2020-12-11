1d690dfbb6d9674779a52d71cf115fcc8f85d07f
annajuliapg <annajuliap.g@hotmail.com>
2020-12-11 11:02:32 -0300

    ajustes

--- scripts SQL/SQL Delete.sql
+++ scripts SQL/SQL Delete.sql
@@ -1,3 +1,9 @@
 DELETE FROM usuario_livro
-WHERE idUsuario = 6
+WHERE idUsuario = 1
 AND idLivro = 5;
+
+select * from usuario;
+select * from usuario_livro;
+select * from livro;
+
+UPDATE usuario SET Paginas_Lidas = 0, Livros_Lidos = 0, Tempo_Total_Leitura = 0 WHERE idUsuario = 6;
