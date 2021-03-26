46da0903fd5af03d2afdca67c60a2caf3d8b0d51
Anna Júlia <annajuliap.g@hotmail.com>
2021-03-25 21:42:09 -0300

    select com varios autores

--- back-end/db.js
+++ back-end/db.js
@@ -60,7 +60,7 @@ async function selectUsuario(idUsuario) {
 async function selectLivros() {
     const conn = await connect();
 
-    const [rows] = await conn.query("SELECT l.idLivro, l.Nome_Livro, a.Nome_Autor, l.Total_Paginas, l.Ano_Lancamento, g.Nome_Genero FROM livro l INNER JOIN genero g ON l.idGenero = g.idGenero INNER JOIN livro_autor la ON l.idLivro = la.idLivro INNER JOIN autor a ON la.idAutor = a.idAutor;");
+    const [rows] = await conn.query("SELECT DISTINCT l.idLivro, l.Nome_Livro, (select GROUP_CONCAT(a.Nome_Autor SEPARATOR ', ') from autor a INNER JOIN livro_autor la ON l.idLivro = la.idLivro AND la.idAutor = a.idAutor) AS Nome_Autor, l.Total_Paginas, l.Ano_Lancamento, g.Nome_Genero FROM livro l INNER JOIN genero g ON l.idGenero = g.idGenero;");
 
     if (!rows.length) return -1
     else return rows;
@@ -70,7 +70,7 @@ async function selectLivros() {
 async function selectLivrosNaoTem(idUsuario) {
     const conn = await connect();
 
-    const [rows] = await conn.query("SELECT l.idLivro, l.Nome_Livro, a.Nome_Autor, l.Total_Paginas, l.Ano_Lancamento, g.Nome_Genero FROM livro l INNER JOIN genero g ON l.idGenero = g.idGenero INNER JOIN livro_autor la ON l.idLivro = la.idLivro INNER JOIN autor a ON la.idAutor = a.idAutor WHERE l.idLivro NOT IN ( SELECT idLivro FROM usuario_livro WHERE idUsuario = ?) ORDER BY l.idLivro;", idUsuario);
+    const [rows] = await conn.query("SELECT l.idLivro, l.Nome_Livro, (select GROUP_CONCAT(a.Nome_Autor SEPARATOR ', ') from autor a INNER JOIN livro_autor la ON l.idLivro = la.idLivro AND la.idAutor = a.idAutor) AS Nome_Autor, l.Total_Paginas, l.Ano_Lancamento, g.Nome_Genero FROM livro l INNER JOIN genero g ON l.idGenero = g.idGenero WHERE l.idLivro NOT IN ( SELECT idLivro FROM usuario_livro WHERE idUsuario = ?) ORDER BY l.idLivro;", idUsuario);
 
     if (!rows.length) return -1
     else return rows;
--- scripts SQL/SELECT VARIOS AUTORES.sql
+++ scripts SQL/SELECT VARIOS AUTORES.sql
@@ -3,5 +3,5 @@ SELECT DISTINCT l.idLivro, l.Nome_Livro,
 	select GROUP_CONCAT(a.Nome_Autor SEPARATOR ', ') 
     from autor a 
     INNER JOIN livro_autor la ON l.idLivro = la.idLivro AND la.idAutor = a.idAutor
-) AS AUTORES
+) AS Nome_Autor
 FROM livro l
